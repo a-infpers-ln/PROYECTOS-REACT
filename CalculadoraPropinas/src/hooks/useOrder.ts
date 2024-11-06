163b8b322c0c516a67399a109545b6fda43a319f
DESKTOP-3A7V4IJ\Alejandro <a.infpers.ln@gmail.com>
2024-11-06 13:30:35 +0100

    Creando la función para añadir articulos a la orden

--- CalculadoraPropinas/src/hooks/useOrder.ts
+++ CalculadoraPropinas/src/hooks/useOrder.ts
@@ -3,10 +3,11 @@ import type { OrderItem } from "../types"
 
 export default function useOrder() {
     const [order, setOrder] = useState<OrderItem[]>([])
-    const [total, setTotal] = useState(0)
-    const [auth, setAuth] = useState(false)
+    const addItem = () => {
+        console.log("Agregando...")
+    }
 
     return {
-
+        addItem
     }
 }
