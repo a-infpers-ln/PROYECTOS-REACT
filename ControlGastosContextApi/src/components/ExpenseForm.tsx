82731314eced29217765d671f1a3e02a86c5d61f
DESKTOP-3A7V4IJ\Alejandro <a.infpers.ln@gmail.com>
2025-01-03 14:05:08 +0100

    Editando el gasto

--- ControlGastosContextApi/src/components/ExpenseForm.tsx
+++ ControlGastosContextApi/src/components/ExpenseForm.tsx
@@ -51,8 +51,12 @@ export default function ExpenseForm() {
             setError('Todos los campos son obligatorios')
             return 
         }
-        //Agregar un nuevo gasto
-        dispatch({type: 'add-expense', payload: {expense}})
+        //Agregar un nuevo gasto o actualizarlo
+        if(state.editingId){
+            dispatch({type: 'update-expense', payload: {expense: {id: state.editingId, ...expense}}})
+        }else{
+            dispatch({type: 'add-expense', payload: {expense}})
+        }
 
         //Reiniciar
         setExpense({
