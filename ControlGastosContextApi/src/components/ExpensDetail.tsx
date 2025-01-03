276248ca0a7c68383b4c5af218e0081cfe0d418b
DESKTOP-3A7V4IJ\Alejandro <a.infpers.ln@gmail.com>
2025-01-03 11:42:51 +0100

    Mostrando los gastos

--- ControlGastosContextApi/src/components/ExpensDetail.tsx
+++ ControlGastosContextApi/src/components/ExpensDetail.tsx
@@ -1,11 +1,26 @@
+import { formatDate } from "../helpers"
 import { Expense } from "../types"
+import AmountDisplay from "./AmountDisplay"
 
 type ExpenseDetailProps = {
-    expense : Expensee
+    expense : Expense
 }
 
 export default function ExpensDetail({expense} : ExpenseDetailProps) {
   return (
-    <div>ExpensDetail</div>
+    <div className="bg-white shadow-lg p-10 w-full border-b border-gray-200 flex gap-5 items-center">
+        <div>
+
+        </div>
+
+        <div>
+            <p>{expense.expenseName}</p>
+            <p className="text-slate-600 text-sm">{formatDate(expense.date!.toString())}</p>
+        </div>
+
+        <AmountDisplay 
+            amount={expense.amount} 
+        />
+    </div>
   )
 }
