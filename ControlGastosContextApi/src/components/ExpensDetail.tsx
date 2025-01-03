ff8402af95c3a979b00fb75ec7363ab33632aebf
DESKTOP-3A7V4IJ\Alejandro <a.infpers.ln@gmail.com>
2025-01-03 11:32:29 +0100

    Creando el componente para mostrar los gastos

---
+++ ControlGastosContextApi/src/components/ExpensDetail.tsx
@@ -0,0 +1,11 @@
+import { Expense } from "../types"
+
+type ExpenseDetailProps = {
+    expense : Expensee
+}
+
+export default function ExpensDetail({expense} : ExpenseDetailProps) {
+  return (
+    <div>ExpensDetail</div>
+  )
+}
