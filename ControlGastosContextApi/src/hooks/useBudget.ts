eb278da898e28358d863982141913b1a849a1f08
DESKTOP-3A7V4IJ\Alejandro <a.infpers.ln@gmail.com>
2025-01-03 13:29:50 +0100

    Agregando interacción de Swipe a los gastos para Editar y Eliminar

--- ControlGastosContextApi/src/hooks/useBudget.ts
+++ ControlGastosContextApi/src/hooks/useBudget.ts
@@ -1,12 +1,10 @@
-import { useContext } from "react";
-import { BudgetContext } from "../context/BudgetContext";
+import { useContext } from "react"
+import { BudgetContext } from "../context/BudgetContext"
 
 export const useBudget = () => {
     const context = useContext(BudgetContext)
-
-    if(!context){
+    if(!context) {
         throw new Error('useBudget must be used within a BudgetProvider')
     }
-
     return context
 }
