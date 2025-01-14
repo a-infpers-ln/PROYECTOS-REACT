c5cb7df8faefbb9420494101d31c878818e0924b
DESKTOP-3A7V4IJ\Alejandro <a.infpers.ln@gmail.com>
2025-01-14 11:33:31 +0100

    Escribiendo en el estado de Zustand

--- PacientesZustand/src/components/PatientsList.tsx
+++ PacientesZustand/src/components/PatientsList.tsx
@@ -1,4 +1,7 @@
+import { usePatientStore } from "../store"
+
 export default function PatientsList() {
+    const patients = usePatientStore(state => state.patients)
   return (
     <div>PatientsList</div>
   )
