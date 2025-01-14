ad933d28cd3a4c4875d64f05c32d7792dca0602f
DESKTOP-3A7V4IJ\Alejandro <a.infpers.ln@gmail.com>
2025-01-14 16:54:40 +0100

    Mostrar Toast en Editar y Eliminar

--- PacientesZustand/src/components/PatientDetails.tsx
+++ PacientesZustand/src/components/PatientDetails.tsx
@@ -2,6 +2,7 @@
 import { Patient } from "../types"
 import PatientDetailItem from "./PatientDetailItem"
 import { usePatientStore } from "../store"
+import { toast } from "react-toastify"
 
 type PatientDetailsProps = {
     patient: Patient
@@ -11,6 +12,13 @@ export default function PatientDetails({patient} : PatientDetailsProps) {
 
     const detelePatient = usePatientStore((state) => state.deletePatient)
     const getPatientById = usePatientStore((state) => state.getPatientById)
+
+    const hundleClick = () => {
+        detelePatient(patient.id)
+        toast('Paciente Eliminado', {
+            type: 'error'
+        })
+    }
 
   return (
     <div className="mx-5 my-10 px-5 py-10 bg-white shadow-md rounded-xl">
@@ -34,7 +42,7 @@ export default function PatientDetails({patient} : PatientDetailsProps) {
             <button
                 type="button"
                 className="py-2 px-10 bg-red-600 hover:bg-red-700 text-white font-bold uppercase rounded-lg"
-                onClick={() => detelePatient(patient.id)}
+                onClick={hundleClick}
             >
                 Eliminar
             </button>
