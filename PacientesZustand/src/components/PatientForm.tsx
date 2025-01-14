1d5b93bed90fe6dc74bd829865349f4d2cf3d251
DESKTOP-3A7V4IJ\Alejandro <a.infpers.ln@gmail.com>
2025-01-14 11:01:10 +0100

    Agregando validación a los otros campos

--- PacientesZustand/src/components/PatientForm.tsx
+++ PacientesZustand/src/components/PatientForm.tsx
@@ -50,7 +50,13 @@ export default function PatientForm() {
                       className="w-full p-3  border border-gray-100"  
                       type="text" 
                       placeholder="Nombre del Propietario" 
+                      {...register('caretaker', {
+                        required: 'El Propietario es obligatorio'
+                    })}
                   />
+                  {errors.caretaker && (
+                    <Error>{errors.caretaker?.message?.toString()}</Error>
+                  )}
                 </div>
   
               <div className="mb-5">
@@ -62,7 +68,17 @@ export default function PatientForm() {
                     className="w-full p-3  border border-gray-100"  
                     type="email" 
                     placeholder="Email de Registro" 
+                    {...register("email", {
+                        required: "El Email es Obligatorio",
+                        pattern: {
+                          value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                          message: 'Email No Válido'
+                        }
+                      })} 
                 />
+                {errors.email && (
+                    <Error>{errors.email?.message?.toString()}</Error>
+                )}
               </div>
   
               <div className="mb-5">
@@ -73,7 +89,13 @@ export default function PatientForm() {
                       id="date"
                       className="w-full p-3  border border-gray-100"  
                       type="date" 
-                  />
+                      {...register('date', {
+                        required: 'La fecha de alta es obligatoria'
+                    })}
+                />
+                {errors.date && (
+                    <Error>{errors.date?.message?.toString()}</Error>
+                )}
               </div>
               
               <div className="mb-5">
@@ -84,7 +106,13 @@ export default function PatientForm() {
                       id="symptoms"
                       className="w-full p-3  border border-gray-100"  
                       placeholder="Síntomas del paciente" 
+                      {...register('symptoms', {
+                        required: 'Los sintomas son obligatorios'
+                    })}
                   ></textarea>
+                {errors.symptoms && (
+                    <Error>{errors.symptoms?.message?.toString()}</Error>
+                )}
               </div>
   
               <input
