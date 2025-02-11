ecf7ecea4f51537f36a7b130d29e2fcbd5d40b6a
DESKTOP-3A7V4IJ\Alejandro <a.infpers.ln@gmail.com>
2025-02-11 11:02:19 +0100

    Primre Llamado hacia la API

--- clima-react-typescript/src/hooks/useWeather.ts
+++ clima-react-typescript/src/hooks/useWeather.ts
@@ -1,5 +1,19 @@
+import axios from "axios"
+import { SearchType } from "../types"
+
 export default function useWeather() {
-    const fetchWeather = () => {
+    const fetchWeather = async (search: SearchType) => {
+
+        const appId = 'ff734e22c41352507c764134bc320565'
+        try {
+                
+            const geoUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${search.city},${search.country}&appid=~${appId}`
+
+            const data = await axios.get(geoUrl)
+            
+        } catch (error) {
+            console.log(error)
+        }
 
     }
 
