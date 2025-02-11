6b3bab2f034efb42c03344e004e603645dc285ef
DESKTOP-3A7V4IJ\Alejandro <a.infpers.ln@gmail.com>
2025-02-11 11:50:26 +0100

    Tipar Resultados de una API - Validar con Valibot

--- clima-react-typescript/src/hooks/useWeather.ts
+++ clima-react-typescript/src/hooks/useWeather.ts
@@ -1,5 +1,6 @@
 import axios from "axios"
-import {z} from 'zod'
+//import {z} from 'zod'
+import {object, string, number, InferOutput, parse} from 'valibot'
 import { SearchType } from "../types"
 
 //TYPE GUARD O ASSERTION
@@ -16,16 +17,28 @@ import { SearchType } from "../types"
 }*/
 
 //Zod
-const Weather = z.object({
+/*const Weather = z.object({
     name: z.string(),
     main: z.object({
         temp: z.number(),
         temp_max: z.number(),
         temp_min: z.number()
     })
-})
+})*/
 
-type Weather = z.infer<typeof Weather>
+//type Weather = z.infer<typeof Weather>
+
+//Valibot
+const WeatherSchema = object({
+    name: string(),
+    main: object({
+        temp: number(),
+        temp_max: number(),
+        temp_min: number()
+
+    })
+})
+type Weather = InferOutput<typeof WeatherSchema>
 
 export default function useWeather() {
     const fetchWeather = async (search: SearchType) => {
@@ -56,9 +69,19 @@ export default function useWeather() {
             }*/
 
             //Zod
-            const { data: weatherResult } = await axios<Weather>(wetherUrl)
+            /*const { data: weatherResult } = await axios<Weather>(wetherUrl)
             const result = Weather.safeParse(weatherResult)
             if(result.success) {
+
+            }else{
+                console.log('Respuesta mal formada')
+            }*/
+
+            //Valibot
+            const { data: weatherResult } = await axios<Weather>(wetherUrl)
+            const result = parse(WeatherSchema, weatherResult)
+
+            if(result) {
 
             }else{
                 console.log('Respuesta mal formada')
