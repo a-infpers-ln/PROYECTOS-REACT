56bd027df53c5a8ebbd7e6594bee557949b8c648
DESKTOP-3A7V4IJ\Alejandro <a.infpers.ln@gmail.com>
2025-02-11 11:09:13 +0100

    Variables de Entorno

--- clima-react-typescript/src/hooks/useWeather.ts
+++ clima-react-typescript/src/hooks/useWeather.ts
@@ -4,7 +4,7 @@ import { SearchType } from "../types"
 export default function useWeather() {
     const fetchWeather = async (search: SearchType) => {
 
-        const appId = 'ff734e22c41352507c764134bc320565'
+        const appId = import.meta.env.VITE_API_KEY
         try {
                 
             const geoUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${search.city},${search.country}&appid=~${appId}`
