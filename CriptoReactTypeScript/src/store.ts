a936589085337c93e714df489a5bbc0dcb967334
DESKTOP-3A7V4IJ\Alejandro <a.infpers.ln@gmail.com>
2025-02-20 11:40:35 +0100

    Finalizando la función que obtiene las Criptos

--- CriptoReactTypeScript/src/store.ts
+++ CriptoReactTypeScript/src/store.ts
@@ -2,16 +2,29 @@
 import { create } from 'zustand'
 import axios from 'axios'
 import {CryptoCurrenciesResponseSchema} from './schema/crypto-schema'
+import { Cryptocurrency } from './types'
+
+type CrytoStore = {
+    cryptocurrencies: Cryptocurrency[]
+    fetchCryptos: () => Promise<void>
+}
 
 async function getCryptos() {
     const url = 'https://min-api.cryptocompare.com/data/top/mktcapfull?limit=20&tsym=USD'
     const {data: {data}} = await axios(url)
     const result = CryptoCurrenciesResponseSchema.safeParse(data)
+
+    if(result.success) {
+        return result.data
+    }
 }
 
-export const useCryptoStore = create(() => ({
-    fetchCryptos: () => {
-        getCryptos();
-
+export const useCryptoStore = create<CrytoStore>((set) => ({
+    cryptocurrencies: [],
+    fetchCryptos: async () => {
+        const cryptocurrencies = await getCryptos()
+        set(() => ({
+            cryptocurrencies
+        }))
     }
 }))
