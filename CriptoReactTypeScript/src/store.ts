2bc1c9346f14e61b42ec507ae9cf93ef27fa5f23
DESKTOP-3A7V4IJ\Alejandro <a.infpers.ln@gmail.com>
2025-02-20 11:27:05 +0100

    Validando la respuesta de la API con Zod

--- CriptoReactTypeScript/src/store.ts
+++ CriptoReactTypeScript/src/store.ts
@@ -1,10 +1,12 @@
 
 import { create } from 'zustand'
 import axios from 'axios'
+import {CryptoCurrenciesResponseSchema} from './schema/crypto-schema'
 
 async function getCryptos() {
     const url = 'https://min-api.cryptocompare.com/data/top/mktcapfull?limit=20&tsym=USD'
     const {data: {data}} = await axios(url)
+    const result = CryptoCurrenciesResponseSchema.safeParse(data)
 }
 
 export const useCryptoStore = create(() => ({
