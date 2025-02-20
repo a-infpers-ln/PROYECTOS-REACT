ab8733fd46749c4c250862fe0b5f9ee46adc1aa4
DESKTOP-3A7V4IJ\Alejandro <a.infpers.ln@gmail.com>
2025-02-20 13:56:34 +0100

    Mostrando el Resultado de la cotización, agregando CSS al resultado de la cotización y agregando un spinner de Carga + Correción de error en CriptoSearchForm

--- CriptoReactTypeScript/src/components/CriptoSearchForm.tsx
+++ CriptoReactTypeScript/src/components/CriptoSearchForm.tsx
@@ -1,87 +1,79 @@
-import { useState } from "react"
-import { currencies } from "../data"
 import { useCryptoStore } from "../store"
+import { currencies } from "../data"
+import { ChangeEvent, useState } from "react"
 import { Pair } from "../types"
 import ErrorMessage from "./ErrorMessage"
 
 export default function CriptoSearchForm() {
-  const cryptocurrencies = useCryptoStore((state) => state.cryptocurrencies)
-  const fetchData = useCryptoStore((state) => state.fetchData)
+    const cryptocurrencies = useCryptoStore((state) => state.cryptocurrencies)
+    const fetchData = useCryptoStore((state) => state.fetchData)
 
-
-  const [pair, setPair] = useState<Pair>({
-    currency: '',
-    criptocurrency: ''
-  })
-  const [error, setError] = useState('')
-
-  const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
-    setPair({
-      ...pair,
-      [e.target.name]: e.target.value
+    const [pair, setPair] = useState<Pair>({
+        currency: '',
+        criptocurrency: ''
     })
-  }
-
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    if(Object.values(pair).includes('')) {
-      setError('Todos los campos son obligatorios')
-      return
+    const [error, setError] = useState('')
+    
+    const handleChange = (e: ChangeEvent<HTMLSelectElement>) => {
+        setPair({
+            ...pair,
+            [e.target.name]: e.target.value
+        })
     }
 
-    setError('')
-    fetchData(pair)
-  }
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        if(Object.values(pair).includes('')) {
+            setError('Todos los campos son obligatorios')
+            return
+        }
+        setError('')
+        fetchData(pair)
+    }
 
-  return (
-    <form
-      className='form'
-      onSubmit={handleSubmit}
-    >
 
-        {error && <ErrorMessage>{error}</ErrorMessage>}
+    return (
+        <form
+            className='form'
+            onSubmit={handleSubmit}
+        >
 
-        <div className='field'>
-            <label htmlFor="currency">Moneda:</label>
-            <select
-                name="currency"
-                id="currency"
-                onChange={handleChange}
-                value={pair.currency}
-            >
-                <option value="">-- Seleccione --</option>
-                {currencies.map(currency => (
-                  <option 
-                    key={currency.code} 
-                    value={currency.code}>
-                      {currency.name}
-                  </option>
-                ))}
-            </select>
-        </div>
+            {error && <ErrorMessage>{error}</ErrorMessage>}
 
-        <div className='field'>
-            <label htmlFor="criptomoneda">Criptomoneda:</label>
-            <select
-                name="criptomoneda"
-                id="criptomoneda"
-                onChange={handleChange}
-                value={pair.criptocurrency}
-            >
-                <option value="">-- Seleccione --</option>
-                {cryptocurrencies.map((crypto) => (
-                  <option 
-                    key={crypto.CoinInfo.FullName} 
-                    value={crypto.CoinInfo.Name}
-                  >
-                    {crypto.CoinInfo.FullName}
-                  </option>
-                ))}
+            <div className='field'>
+                <label htmlFor="currency">Moneda:</label>
+                <select 
+                    name="currency" 
+                    id="currency"
+                    onChange={handleChange}
+                    value={pair.currency}
+                >
+                    <option value="">-- Seleccione --</option>
+                    {currencies.map( currency => (
+                        <option key={currency.code} value={currency.code}>{currency.name}</option>
+                    ))}
+                </select>
+            </div>
 
-            </select>
-        </div>
+            <div className='field'>
+                <label htmlFor="criptocurrency">Criptomoneda:</label>
+                <select 
+                    name="criptocurrency" 
+                    id="criptocurrency"
+                    onChange={handleChange}
+                    value={pair.criptocurrency}
+                >
+                    <option value="">-- Seleccione --</option>
+                    {cryptocurrencies.map( crypto => (
+                        <option
+                            key={crypto.CoinInfo.FullName}
+                            value={crypto.CoinInfo.Name}
+                        >{crypto.CoinInfo.FullName}</option>
+                    ))}
+                </select>
+            </div>
 
-        <input type='submit' value='Cotizar' />
-    </form>
-  )
+            <input type='submit' value='Cotizar' />
+        </form>
+    )
 }
--- CriptoReactTypeScript/src/store.ts
+++ CriptoReactTypeScript/src/store.ts
@@ -1,19 +1,27 @@
-
 import { create } from 'zustand'
 import { devtools } from 'zustand/middleware'
-import { Cryptocurrency, CryptoPrice, Pair } from './types'
+import { CryptoPrice, Cryptocurrency, Pair } from './types'
 import { getCryptos, fetchCurrentCryptoPrice } from './services/CryptoService'
 
-type CrytoStore = {
+type CryptoStore = {
     cryptocurrencies: Cryptocurrency[]
     result: CryptoPrice
+    loading: boolean
     fetchCryptos: () => Promise<void>
-    fetchData: (pair: Pair ) => Promise<void>
+    fetchData: (pair: Pair) => Promise<void>
 }
 
-export const useCryptoStore = create<CrytoStore>()(devtools((set) => ({
+export const useCryptoStore = create<CryptoStore>()(devtools((set) => ({
     cryptocurrencies: [],
-    result: {} as CryptoPrice,
+    result: {
+        IMAGEURL : '',
+        PRICE: '',
+        HIGHDAY: '',
+        LOWDAY: '',
+        CHANGEPCT24HOUR: '',
+        LASTUPDATE: ''
+    },
+    loading: false,
     fetchCryptos: async () => {
         const cryptocurrencies = await getCryptos()
         set(() => ({
@@ -21,9 +29,13 @@ export const useCryptoStore = create<CrytoStore>()(devtools((set) => ({
         }))
     },
     fetchData: async (pair) => {
+        set(() => ({
+            loading: true
+        }))
         const result = await fetchCurrentCryptoPrice(pair)
         set(() => ({
-            result
+            result,
+            loading: false
         }))
     }
 })))
