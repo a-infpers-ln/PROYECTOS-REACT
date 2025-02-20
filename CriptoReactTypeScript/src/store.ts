d97b025e8c837eb3212d5b4f042d2eef77fe7770
DESKTOP-3A7V4IJ\Alejandro <a.infpers.ln@gmail.com>
2025-02-20 11:20:09 +0100

    Obteniendo las Criptomonedas en Zustand

--- CriptoReactTypeScript/src/store.ts
+++ CriptoReactTypeScript/src/store.ts
@@ -1,8 +1,15 @@
 
 import { create } from 'zustand'
+import axios from 'axios'
+
+async function getCryptos() {
+    const url = 'https://min-api.cryptocompare.com/data/top/mktcapfull?limit=20&tsym=USD'
+    const {data: {data}} = await axios(url)
+}
 
 export const useCryptoStore = create(() => ({
     fetchCryptos: () => {
-        
+        getCryptos();
+
     }
 }))
