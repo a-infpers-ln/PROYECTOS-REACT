14f4137bf7806a9a264719d61beaed57f9cce8c8
DESKTOP-3A7V4IJ\Alejandro <a.infpers.ln@gmail.com>
2025-04-21 13:05:49 +0200

    Buscar las Recetas en la base a categoria e ingredientes

--- BebidasReact/src/services/RecipeService.ts
+++ BebidasReact/src/services/RecipeService.ts
@@ -1,8 +1,17 @@
 import axios from "axios";
 import { CategoriesAPIResponseSchema } from "../utils/recipes-schema";
+import { SearchFilter } from "../types";
 
 export async function getCategories() {
     const url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list'
+    const {data} = await axios(url)
+    const result = CategoriesAPIResponseSchema.safeParse(data)
+    if(result.success) {
+        return result.data
+    }
+}
+export async function getRecipes(filters: SearchFilter) {
+    const url = `http://www.thecocktaildb.com/api/json/v1/1/filter.php?c=${filters.category}&${filters.ingredient}`
     const {data} = await axios(url)
     const result = CategoriesAPIResponseSchema.safeParse(data)
     if(result.success) {
