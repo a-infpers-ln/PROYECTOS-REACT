cb426058ad6bb7bc6d7ba0be1560fed807c29aa4
DESKTOP-3A7V4IJ\Alejandro <a.infpers.ln@gmail.com>
2025-05-07 13:31:40 +0200

    Diferentes Métodos HTTP y probando con ThunderClient y Postman

--- RestApiNodeTsServer/src/server.ts
+++ RestApiNodeTsServer/src/server.ts
@@ -3,12 +3,24 @@ import express from "express";
 const server = express()
 
 //Routing
+server.get('/', (req, res) => {
+  res.json('Desde GET')
+})
+
 server.post('/', (req, res) => {
-  const datos = [
-    {id: 1, nombre: 'Alex'},
-    {id: 2, nombre: 'Juan'}
-  ]
-    res.send(datos)
+  res.json('Desde POST')
+})
+
+server.put('/', (req, res) => {
+  res.json('Desde PUT')
+})
+
+server.patch('/', (req, res) => {
+  res.json('Desde PATCH')
+})
+
+server.delete('/', (req, res) => {
+  res.json('Desde DELETE')
 })
 
 export default server
